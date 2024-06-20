372b6bb0674e50b93c2aee32af820eda91307f32
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-06-20 18:49:20 +0000

    feat: :sparkles: Agregar consultas

--- dia_02/mysql2_ejercio2.sql
+++ dia_02/mysql2_ejercio2.sql
@@ -36,32 +36,148 @@ from empleado;
 -- 3. Lista todas las columnas de la tabla empleado.
 select *
 from empleado;
-select *
-from information_schema.columns empleado;
+-- or...
+show COLUMNS
+from mysql2_dia02_ejercicio2.empleado;
 
 -- 4. Lista el nombre y los apellidos de todos los empleados.
-
+select nombre as Nombre, apellido1 as Apellido_1, apellido2 as Apellido_2
+from empleado;
 
 -- 5. Lista el identificador de los departamentos de los empleados que aparecen en la tabla empleado.
-
+select id_departamento as Identificadores
+from empleado
+where id_departamento is not null;
 
 -- 6. Lista el identificador de los departamentos de los empleados que aparecen en la tabla
 -- empleado, eliminando los identificadores que aparecen repetidos.
-
+select distinct id_departamento as Identificadores
+from empleado
+where id_departamento is not null;
 
 -- 7. Lista el nombre y apellidos de los empleados en una única columna.
+delimiter //
+create function Nombre_Apellidos(nombre varchar(100),apellido1 varchar(100), apellido2 varchar(100))
+returns varchar(255) deterministic
+begin
+	if apellido2 is null then
+    	return CONCAT(nombre," ",apellido1);
+	else
+		return CONCAT(nombre," ",apellido1," ",apellido2);
+	end if;
+end//
+delimiter ;
 
+select Nombre_Apellidos(e.nombre,e.apellido1,e.apellido2)
+from empleado e;
 
 -- 8. Lista el nombre y apellidos de los empleados en una única columna, convirtiendo todos los
 -- caracteres en mayúscula.
+delimiter //
+create function Nombre_Apellidos_Upper(nombre varchar(100),apellido1 varchar(100), apellido2 varchar(100))
+returns varchar(255) deterministic
+begin
+	if apellido2 is null then
+    	return CONCAT(UPPER(nombre)," ",UPPER(apellido1));
+	else
+		return CONCAT(UPPER(nombre)," ",UPPER(apellido1)," ",UPPER(apellido2));
+	end if;
+end//
+delimiter ;
 
+select Nombre_Apellidos_Upper(e.nombre,e.apellido1,e.apellido2)
+from empleado e;
 
 -- 9. Lista el nombre y apellidos de los empleados en una única columna, convirtiendo todos los
 -- caracteres en minúscula.
+delimiter //
+create function Nombre_Apellidos_Lower(nombre varchar(100),apellido1 varchar(100), apellido2 varchar(100))
+returns varchar(255) deterministic
+begin
+	if apellido2 is null then
+    	return CONCAT(Lower(nombre)," ",Lower(apellido1));
+	else
+		return CONCAT(Lower(nombre)," ",Lower(apellido1)," ",Lower(apellido2));
+	end if;
+end//
+delimiter ;
 
+select Nombre_Apellidos_Lower(e.nombre,e.apellido1,e.apellido2)
+from empleado e;
 
 -- 10. Lista el identificador de los empleados junto al nif, pero el nif deberá aparecer en dos
 -- columnas, una mostrará únicamente los dígitos del nif y la otra la letra.
+DELIMITER // -- BORRAR TODOS LOS NÚMEROS DEL NIF
+CREATE FUNCTION nif_text(nif varchar(9)) 
+RETURNS varchar(9) deterministic
+BEGIN
+    DECLARE ctrNumber varchar(50);
+    DECLARE finText text default ' ';
+    DECLARE sChar varchar(2);
+    DECLARE inti INTEGER default 1;
+    IF length(nif) > 0 THEN
+        WHILE(inti <= length(nif)) DO -- ITERAR EN NIF
+            SET sChar= SUBSTRING(nif,inti,1);
+            SET ctrNumber= FIND_IN_SET(sChar,'0,1,2,3,4,5,6,7,8,9');
+
+            IF ctrNumber = 0 THEN
+               SET finText=CONCAT(finText,sChar);
+            ELSE
+               SET finText=CONCAT(finText,'');
+            END IF;
+            SET inti=inti+1;
+        END WHILE;
+        RETURN finText;
+    ELSE
+        RETURN '';
+    END IF;
+END//
+DELIMITER ;
+DELIMITER // -- BORRAR TODOS LAS LETRAS DEL NIF
+CREATE FUNCTION nif_num(nif varchar(9)) 
+RETURNS varchar(9) deterministic
+BEGIN
+    DECLARE ctrNumber varchar(50);
+    DECLARE finText text default ' ';
+    DECLARE sChar varchar(2);
+    DECLARE inti INTEGER default 1;
+    IF length(nif) > 0 THEN
+        WHILE(inti <= length(nif)) DO -- ITERAR EN NIF
+            SET sChar= SUBSTRING(nif,inti,1);
+            SET ctrNumber= FIND_IN_SET(sChar,'q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,ñ,z,x,c,v,b,n,m');
+
+            IF ctrNumber = 0 THEN
+               SET finText=CONCAT(finText,sChar);
+            ELSE
+               SET finText=CONCAT(finText,'');
+            END IF;
+            SET inti=inti+1;
+        END WHILE;
+        RETURN finText;
+    ELSE
+        RETURN '';
+    END IF;
+END//
+DELIMITER ;
+
+select id as ID, nif_num(nif) as NIF_Num, nif_text(nif) as NIF_Letter
+from empleado
+order by id;
+
+
+-- 11. Lista el nombre de cada departamento y el valor del presupuesto actual del que dispone. Para calcular este dato tendrá
+-- que restar al valor del presupuesto inicial (columna presupuesto) los gastos que se han generado (columna gastos). Tenga
+-- en cuenta que en algunos casos pueden existir valores negativos. Utilice un alias apropiado para la nueva columna columna
+-- que está calculando.
+
+
+-- 12. Lista el nombre de los departamentos y el valor del presupuesto actual ordenado de forma ascendente.
+
+
+-- 13. Lista el nombre de todos los departamentos ordenados de forma ascendente.
+
+
+-- 14. Lista el nombre de todos los departamentos ordenados de forma descendente.
 
 
 -- Insertar información departamento
