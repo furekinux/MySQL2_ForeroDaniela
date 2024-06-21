5c932f2cc621b275acc4c2632a7092d733a42855
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-06-21 13:59:32 +0000

    feat: :sparkles: Agregar enunciado y organizar

--- dia_02/mysql2_ejercio2.sql
+++ dia_02/mysql2_ejercio2.sql
@@ -547,29 +547,51 @@ end //
 delimiter ;
 call E_Info_Dep_Busc3(2,4,5);
 
+-- ############ CONSULTAS MULTITABLA ############
+-- 1. Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno.
+
+-- 2. Devuelve un listado con los empleados y los datos de los departamentos donde trabaja cada uno. Ordena el resultado, en primer lugar por el nombre del departamento (en orden alfabético) y en segundo lugar por los apellidos y el nombre de los empleados.
+
+-- 3. Devuelve un listado con el identificador y el nombre del departamento, solamente de aquellos departamentos que tienen empleados.
+
+-- 4. Devuelve un listado con el identificador, el nombre del departamento y el valor del presupuesto actual del que dispone, solamente de aquellos departamentos que tienen empleados. El valor del presupuesto actual lo puede calcular restando al valor del presupuesto inicial (columna presupuesto) el valor de los gastos que ha generado(columna gastos).
+
+-- 5. Devuelve el nombre del departamento donde trabaja el empleado que tiene el nif 38382980M.
+
+-- 6. Devuelve el nombre del departamento donde trabaja el empleado Pepe Ruiz Santana.
+
+-- 7. Devuelve un listado con los datos de los empleados que trabajan en el departamento de I+D. Ordena el resultado alfabéticamente.
+
+-- 8. Devuelve un listado con los datos de los empleados que trabajan en el departamento de Sistemas, Contabilidad o I+D. Ordena el resultado alfabéticamente.
+
+-- 9. Devuelve una lista con el nombre de los empleados que tienen los departamentos que no tienen un presupuesto entre 100000 y 200000 euros.
+
+-- 10. Devuelve un listado con el nombre de los departamentos donde existe algún empleado cuyo segundo apellido sea NULL. Tenga en cuenta que no debe mostrar nombres de departamentos que estén repetidos.
+
 -- Insertar información departamento
-INSERT INTO departamento VALUES(1, 'Desarrollo', 120000, 6000);
-INSERT INTO departamento VALUES(2, 'Sistemas', 150000, 21000);
-INSERT INTO departamento VALUES(3, 'Recursos Humanos', 280000, 25000);
-INSERT INTO departamento VALUES(4, 'Contabilidad', 110000, 3000);
-INSERT INTO departamento VALUES(5, 'I+D', 375000, 380000);
-INSERT INTO departamento VALUES(6, 'Proyectos', 0, 0);
-INSERT INTO departamento VALUES(7, 'Publicidad', 0, 1000);
+INSERT INTO departamento VALUES
+    (1, 'Desarrollo', 120000, 6000),
+    (2, 'Sistemas', 150000, 21000),
+    (3, 'Recursos Humanos', 280000, 25000),
+    (4, 'Contabilidad', 110000, 3000),
+    (5, 'I+D', 375000, 380000),
+    (6, 'Proyectos', 0, 0),
+    (7, 'Publicidad', 0, 1000);
 
 -- Insertar información empleado
-INSERT INTO empleado VALUES(1, '32481596F', 'Aarón', 'Rivero', 'Gómez', 1);
-INSERT INTO empleado VALUES(2, 'Y5575632D', 'Adela', 'Salas', 'Díaz', 2);
-INSERT INTO empleado VALUES(3, 'R6970642B', 'Adolfo', 'Rubio', 'Flores', 3);
-INSERT INTO empleado VALUES(4, '77705545E', 'Adrián', 'Suárez', NULL, 4);
-INSERT INTO empleado VALUES(5, '17087203C', 'Marcos', 'Loyola', 'Méndez', 5);
-INSERT INTO empleado VALUES(6, '38382980M', 'María', 'Santana', 'Moreno', 1);
-INSERT INTO empleado VALUES(7, '80576669X', 'Pilar', 'Ruiz', NULL, 2);
-INSERT INTO empleado VALUES(8, '71651431Z', 'Pepe', 'Ruiz', 'Santana', 3);
-INSERT INTO empleado VALUES(9, '56399183D', 'Juan', 'Gómez', 'López', 2);
-INSERT INTO empleado VALUES(10, '46384486H', 'Diego','Flores', 'Salas', 5);
-INSERT INTO empleado VALUES(11, '67389283A', 'Marta','Herrera', 'Gil', 1);
-INSERT INTO empleado VALUES(12, '41234836R', 'Irene','Salas', 'Flores', NULL);
-INSERT INTO empleado VALUES(13, '82635162B', 'Juan Antonio','Sáez', 'Guerrero',
-NULL);
+INSERT INTO empleado VALUES
+    (1, '32481596F', 'Aarón', 'Rivero', 'Gómez', 1),
+    (2, 'Y5575632D', 'Adela', 'Salas', 'Díaz', 2),
+    (3, 'R6970642B', 'Adolfo', 'Rubio', 'Flores', 3),
+    (4, '77705545E', 'Adrián', 'Suárez', NULL, 4),
+    (5, '17087203C', 'Marcos', 'Loyola', 'Méndez', 5),
+    (6, '38382980M', 'María', 'Santana', 'Moreno', 1),
+    (7, '80576669X', 'Pilar', 'Ruiz', NULL, 2),
+    (8, '71651431Z', 'Pepe', 'Ruiz', 'Santana', 3),
+    (9, '56399183D', 'Juan', 'Gómez', 'López', 2),
+    (10, '46384486H', 'Diego','Flores', 'Salas', 5),
+    (11, '67389283A', 'Marta','Herrera', 'Gil', 1),
+    (12, '41234836R', 'Irene','Salas', 'Flores', NULL),
+    (13, '82635162B', 'Juan Antonio','Sáez', 'Guerrero',NULL);
 
 -- Desarrollado por Daniela Forero / ID 1.142.714.225
