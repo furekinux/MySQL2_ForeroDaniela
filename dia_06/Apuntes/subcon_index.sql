6891c25e88cddf8db7bfbcdc0ca5842e695c292f
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-07-04 13:07:29 +0000

    feat: :sparkles: Agregar apuntes día 06

--- dia_06/Apuntes/subcon_index.sql
+++ dia_06/Apuntes/subcon_index.sql
@@ -42,5 +42,60 @@ where Population =
 (select Capital from country where Code = 'JPN'));
 
 -- INDEXACIÓN
+select * from city;
+
+-- CREAR índice en la columna 'Name' de City
+create index idx_city_name on city(Name);
+select * from city;
+select Name from city;
+
+-- CREAR índice compuestro de las columnas 'District' y 'Population'
+create index idx_city_district_population on city(District,Population);
+
+-- Datos estadísticos para ver los índices creados
+SELECT
+    TABLE_NAME,
+    INDEX_NAME,
+    SEQ_IN_INDEX,
+    COLUMN_NAME,
+    CARDINALITY,
+    SUB_PART,
+    INDEX_TYPE,
+    COMMENT
+FROM
+    information_schema.STATISTICS
+WHERE
+    TABLE_SCHEMA = 'mysql2_dia5';
+-- Revisar tamaño de Indexaciones creadas
+SELECT
+    TABLE_NAME,
+    INDEX_LENGTH
+FROM
+    information_schema.TABLES
+WHERE
+    TABLE_SCHEMA = 'mysql2_dia5';
+
+
+-- Transacciones
+-- Secuencias de uno o mas operaciones SQL, las cuales son ejecutadas como unica unidad de trabajo.
+-- en otras palabras, aseguran que todas las operaciones se realicen de manera correcta antes de ser
+-- ejecutadas en la bbdd real, buscando cumplir con las propiedades
+-- ACID, (atomicidad, consistencia, aislamiento, durabilidad)
+
+-- 1º Paso: Inicar transacción(hacen cambios pero no en tiempo real en la BBDD
+start transaction;
+-- 2º Paso: Realizar cambios
+-- EJ: Actualizar la población de la ciudad de 'New York'
+update city
+set population = 9000000
+where Name = 'New York';
+
+select * from city where Name= 'New York';
+
+-- 3º Paso:
+--   Mantener cambios: COMMIT
+--   Revertir cambios: ROLLBACK
+commit;
+rollback;
 
 -- Desarrollado por Daniela Forero Ballén / ID 1.142.714.225
